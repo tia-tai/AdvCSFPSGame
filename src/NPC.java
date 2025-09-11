d610a624f1b0df5d7c41edbddc94916e20c90655
Sam <zliu26@scienceleadership.org>
2025-09-11 09:17:58 -0400

    NPC

--- src/NPC.java
+++ src/NPC.java
@@ -1,2 +1,23 @@
 public class NPC {
+    String NPCname;
+    boolean alive;
+    int kills;
+    int hp;
+    Weapon gun;
+
+    void action(Player player) {
+        float hit = random.nextFloat();
+        if (hit < gun.accuracy) {
+            player.hp -= gun.damage;
+            if (player.hp < 1) {
+                System.out.println("You killed " + player.username);
+                player.alive = false;
+                kills++;
+            } else {
+                System.out.println("You dealt " + gun.damage + "damage to " + player.username);
+            }
+        } else {
+            System.out.println("You have missed");
+        }
+    }
 }
