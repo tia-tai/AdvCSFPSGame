f2fe5e65203c959183ea72dde6380e0764c63fa7
tia-tai <taiyulin14@gmail.com>
2025-09-15 13:04:30 -0400

    added random to NPC

--- src/NPC.java
+++ src/NPC.java
@@ -1,9 +1,13 @@
+import java.util.Random;
+
 public class NPC {
     String NPCname;
     boolean alive = true;
     int kills = 0;
     int hp = 100;
     Weapon gun;
+
+    Random random = new Random();
 
     void action(Player player) {
         if (gun.bulletCount < 1) {
@@ -31,10 +35,10 @@ public class NPC {
         } else {
             float hit = random.nextFloat();
             if (hit < gun.accuracy) {
-                Player.hp -= gun.damage;
-                if (player.hp < 1) {
+                npc.hp -= gun.damage;
+                if (npc.hp < 1) {
                     System.out.println("You killed " + npc.NPCname);
-                    player.alive = false;
+                    npc.alive = false;
                     kills++;
                 } else {
                     System.out.println("You have missed");
