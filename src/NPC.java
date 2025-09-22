67e7a4770e282d57d78c0693257250ff953336c4
Sam <zliu26@scienceleadership.org>
2025-09-22 13:52:13 -0400

    upd

--- src/NPC.java
+++ src/NPC.java
@@ -1,35 +1,47 @@
 public class NPC extends Character { // Sam
 
+    public NPC(String username, int kills, boolean alive, Weapon gun, int hp) {
+        super(username, kills, alive, gun, hp);
+    }
+
+    public NPC() { super();
+    }
+
     void action(Character player) {
         int actionNumber = random.nextInt(0, 2);
         switch (actionNumber) {
             case (0) -> {
-                if (gun.bulletCount < 1) {
+                if (this.getGun().bulletCount < 1) {
                     System.out.println("\n" + this.getUsername() + " needs to reload");
-                    gun.bulletCount = gun.maxBullets;
+                    this.getGun().bulletCount = this.getGun().maxBullets;
                 } else {
                     int bulletsConsumed = random.nextInt(1, 6);
-                    gun.bulletCount = Math.max(0, gun.bulletCount - bulletsConsumed);
+                    this.getGun().bulletCount = Math.max(0, this.getGun().bulletCount - bulletsConsumed);
                     float hit = random.nextFloat();
-                    if (hit < gun.accuracy) {
-                        player.hp -= gun.damage;
-                        if (player.hp < 1) {
-                            System.out.println("\n" + username + " killed " + player.username);
-                            player.alive = false;
-                            kills++;
+                    if (hit < this.getGun().accuracy) {
+                        player.setHp(player.getHp() - this.getGun().damage);
+                        if (player.getHp() < 1) {
+                            System.out.println("\n" + this.getUsername() + " killed " + player.getUsername());
+                            player.setAlive(false);
+                            this.setKills(this.getKills() + 1);
                         } else {
-                            System.out.println("\n" + username + " dealt " + gun.damage + " damage to " + player.username);
+                            System.out.println("\n" + this.getUsername() + " dealt " + this.getGun().damage + " damage to " + player.getUsername());
                         }
                     } else {
-                        System.out.println("\n" + username + " have missed");
+                        System.out.println("\n" + this.getUsername() + " have missed");
                     }
                 }
             } case (1) -> {
                 int healAmount = random.nextInt(10, 50);
-                hp = Math.min(hp + healAmount, 100);
-                System.out.println("\n" + username + " healed " + healAmount + "HP");
+                this.setHp(Math.min(this.getHp() + healAmount, 100));
+                System.out.println("\n" + this.getUsername() + " healed " + healAmount + "HP");
             }
         }
+    }
+    public String toString() {
+        String niceString = "NPC ";
+        niceString += this.getUsername();
+        return niceString;
     }
 }
 
